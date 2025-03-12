2753b0af0eedf2d0aac960f73da1151e4c18fc8a
Rob Jones <rob@wrexham.dev>
2025-03-12 13:33:04 +0000

    testing

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Events\ReverbNotification;
 
 Route::get('/', function () {
-    $message = 'reverb test 11/03';
+    $message = 'reverb test - ' . date('Y-m-d H:i:s');  ;
     broadcast(new ReverbNotification($message));
     return 'triggered';
 });
